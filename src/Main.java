eee48db5e5ba017054bb24664076ddb92ad0eb43
Sobh6n <sobhankhedry7297@gmail.com>
2024-06-23 14:24:50 +0330

    done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        char[] Parks = {'c','p','p','c','c','p','c'};
+        char[] Parks = {'p', 'p', 'c', 'c', 'p', 'c'};
         int l = MaxPark(Parks, 2);
         System.out.println(l);
 
@@ -8,16 +8,20 @@ public class Main {
 
     private static int MaxPark(char[] parks, int k) {
         int ParkedCars = 0;
-        for (int i =0; i< parks.length;i++){
-            if(parks[i]== 'c'){
+        for (int i = 0; i < parks.length; i++) {
+            if (parks[i] == 'p') {
                 continue;
             }
-            for (int j = 0; j < i; j++) {
-                if (parks[j] =='p'){
+            int s = Math.min(0,i-k);
+            for (int j = s; j < k; j++) {
+                if (parks[j] == 'p') {
                     ParkedCars++;
+                    break;
                 }
+
             }
         }
         return ParkedCars;
     }
+
 }
