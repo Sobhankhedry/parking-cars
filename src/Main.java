a40e808e3de49913bb72d305b70a2971c05bdccc
Sobh6n <sobhankhedry7297@gmail.com>
2024-06-23 14:17:29 +0330

    function created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[] Parks = {'c','p','p','c','c','p','c'};
+        int l = MaxPark(Parks, 2);
+        System.out.println(l);
+
+    }
+
+    private static int MaxPark(char[] parks, int k) {
+        int ParkedCars = 0;
+        for (int i =0; i< parks.length;i++){
+            if(parks[i]== 'c'){
+                continue;
+            }
+            for (int j = 0; j < i; j++) {
+                if (parks[j] =='p'){
+                    ParkedCars++;
+                }
+            }
+        }
+        return ParkedCars;
     }
 }
